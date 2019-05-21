a3a3ccafc4a4bf63d6483f111a685578a22aa781
Nordmann.William.1276082880 <william.g.nordmann.ctr@nga.mil>
2019-05-21 11:35:41 -0500

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,98 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import SdkMap from '@boundlessgeo/sdk/components/map';
+import * as SdkMapActions from '@boundlessgeo/sdk/actions/map';
+import { getLayerById } from '@boundlessgeo/sdk/util';
+import { connect } from 'react-redux';
+
+import Slider from '@material-ui/lab/Slider';
+
+class App extends React.Component {
+	componentDidMount() {
+		// add the OSM source
+		this.props.addOsmSource('osm');
+		this.props.addLayer({
+			id: 'osm',
+			source: 'osm'
+		});
+
+		this.props.setView(
+			[
+				-80,
+				40
+			],
+			3
+		);
+
+		this.props.addSource('population', {
+			type: 'raster',
+			tileSize: 256,
+			maxzoom: 15,
+			tiles: [
+				'https://demo.geo-solutions.it/geoserver/topp/wms?service=WMS&version=1.1.0&request=GetMap&layers=topp:states&srs=EPSG:4326&format=image%2Fpng'
+			]
+		});
+		this.props.addLayer({
+			id: 'population',
+			source: 'population',
+			type: 'raster',
+			paint: {
+				'raster-opacity': 0.3
+			}
+		});
+	}
+	updateOpacity(opacity, layer) {
+		this.props.updateLayer('population', {
+			paint: Object.assign({}, layer.paint, {
+				'raster-opacity': opacity / 100
+			})
+		});
+	}
+
+	render() {
+		let layer = '';
+		let opacity = 30;
+		const layers = this.props.map.layers;
+		if (layers.length > 0) {
+			layer = getLayerById(layers, 'population');
+			opacity = layer.paint['raster-opacity'] * 100;
+		}
+		return (
+			<div className="App">
+				<SdkMap className="map" />
+
+				<Slider
+					value={opacity}
+					className="opacitySlide"
+					step={1}
+					aria-labelledby="label"
+					onChange={(event, value) => this.updateOpacity(value, layer)}
+				/>
+			</div>
+		);
+	}
 }
+const mapStateToProps = (state) => ({
+	map: state.map,
+	layer: state.map.layer
+});
+const mapDispatchToProps = (dispatch) => ({
+	updateLayer: (layerName, paintObj) => {
+		dispatch(SdkMapActions.updateLayer(layerName, paintObj));
+	},
+	addLayer: (LayerObj) => {
+		dispatch(SdkMapActions.addLayer(LayerObj));
+	},
+	addSource: (sourceName, SourceObj) => {
+		dispatch(SdkMapActions.addSource(sourceName, SourceObj));
+	},
+	setView: (center, zoom) => {
+		dispatch(SdkMapActions.setView(center, zoom));
+	},
+	addOsmSource: (sourceName) => {
+		dispatch(SdkMapActions.addOsmSource(sourceName));
+	}
+});
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import SdkMapReducer from '@boundlessgeo/sdk/reducers/map';
+import { createStore, combineReducers } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+	combineReducers({
+		map: SdkMapReducer
+	}),
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+ReactDOM.render(
+	<Provider store={store}>
+		<App />{' '}
+	</Provider>,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
